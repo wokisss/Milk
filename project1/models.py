11e9e618c6cd22e673db66359ad12f63036c09e3
wokisss <wokis4131@outlook.com>
2025-12-12 10:57:28 +0800

    修改models

--- project1/models.py
+++ project1/models.py
@@ -9,7 +9,8 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    # 修复：将密码哈希的长度从128增加到256，以存储更安全的哈希值
+    password_hash = db.Column(db.String(256), nullable=False)
     role = db.Column(db.String(20), default='user')
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
 
